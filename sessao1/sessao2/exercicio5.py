e72c2d1366cdbf5c630f27e83f7e23e0c86fc350
MaynaraEMC <maynara.lino@essencemediacom.com>
2024-09-10 14:01:06 +0000

    Adicionando o quinto exercício à sessão 2

---
+++ sessao1/sessao2/exercicio5.py
@@ -0,0 +1,8 @@
+#5 - Faça um algoritmo que digite três valores e  
+#mostre qual foi o maior valor e o menor valor.
+
+lista1 = [1, 2, 3]
+
+maior = max(lista1)
+menor = min(lista1)
+print(maior, menor)
